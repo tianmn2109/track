5eef1bfd84e4a94bbf926279e99990d02a189b40
tianmn2109 <tianmn2109ccst@163.com>
2015-09-28 13:35:34 +0800

    finally

--- track/Controllers/TrackController.cs
+++ track/Controllers/TrackController.cs
@@ -263,7 +263,7 @@ namespace track.Controllers
         //返回各个feature的状态数据
         public List<Feature> getStatus(string stageSelected)
         {
-      /*      string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
+            string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
             SqlConnection connection = new SqlConnection(coonString);
             connection.Open();
             // MessageBox.Show("Open database success!");
@@ -485,8 +485,8 @@ namespace track.Controllers
             //     MessageBox.Show("Close database success !");
             //return View();
 
-     */      /* */
-          List<Feature> list = new List<Feature>();
+           /* */
+     /*     List<Feature> list = new List<Feature>();
             for (int i = 0; i < 100; i++)
             {
                 Feature f1 = new Feature();
@@ -512,7 +512,7 @@ namespace track.Controllers
                 list.Add(f1);
             }
             return list;   
-        }
+       */ }
         public string gjson()
         {
             //  JData obj1 = new JData(new DateTime(2015, 04, 05), 12);
@@ -633,7 +633,7 @@ namespace track.Controllers
                  list.Add(f2);
                  list.Add(f3);
                return list;*/
-     /*    string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
+        string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
             SqlConnection connection = new SqlConnection(coonString);
             connection.Open();
             // MessageBox.Show("Open database success!");
@@ -695,9 +695,9 @@ namespace track.Controllers
             
             connection.Close();
             return list;
-     */      
+         
 
-        StageWeeks f1 = new StageWeeks();
+    /*    StageWeeks f1 = new StageWeeks();
             f1.ID = "001";
             f1.name = "feature 1";
             f1.newWeeks = "4";
@@ -724,7 +724,7 @@ namespace track.Controllers
             list.Add(f3);
             return list;
              
-        }
+      */  }
         //返回每个feature在new active 和exit review 三个阶段的时间
         public ActionResult responseFeatureWeeks()
         {
@@ -767,7 +767,8 @@ namespace track.Controllers
             list.Add(f4);
             list.Add(f5);
             return list;
-        }
+       
+       }
         //获取 prod 阶段的 开放程度 10% 20%等
         public ActionResult responseFeaturePercents()
         {
@@ -913,14 +914,14 @@ namespace track.Controllers
         public List<GoOutTable> queryGoOutTable(string startDate, string endDate, int s)
         {
             
-      /*      string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
+            string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
             SqlConnection connection = new SqlConnection(coonString);
             connection.Open();
             // MessageBox.Show("Open database success!");
             string queryStr = @" SELECT " +
       @" CAST(CWIV.[TfsMigrationTool_ReflectedWorkItemId] as int) as 'ID' , " +
       @"　CWIV.[System_Title] as 'name'　" +
-       @", CWIV.[Microsoft_VSTS_Scheduling_FinishDate] as 'GoOutDate' " +
+       @", CWIV.[ASG_Release_ReleaseDate] as 'GoOutDate' " +
      @" ,DATEPART(WEEK, GETDATE()) - DATEPART(WEEK, dateadd(ms, -1, DATEADD(mm, DATEDIFF(m, 0, getdate()), 0))) + 1 as 'weekOfMonth' " +
      @" ,DATEPART(WEEK, CWIV.[Microsoft_VSTS_Scheduling_FinishDate]) - DATEPART(WEEK, dateadd(ms, -1, DATEADD(mm, DATEDIFF(m, 0, getdate()), 0))) + 1 as 'weekOfClose' " +
     @" FROM[Tfs_Warehouse].[dbo].[CurrentWorkItemView] as CWIV " +
@@ -955,7 +956,7 @@ namespace track.Controllers
                 list.Add(go);
             }
             return list;
-    */              GoOutTable f1 = new GoOutTable();
+    /*              GoOutTable f1 = new GoOutTable();
                      f1.featureName = "feature 1";
                      f1.goOutDate = "8, 6, 2015";
                      f1.week = 1;
@@ -992,7 +993,7 @@ namespace track.Controllers
                      list.Add(f4);
                      list.Add(f5);
                      return list;
-                      
+          */            
         }
         //模拟数据，返回tfs的版本号及对应的时间
         public ActionResult responseTFSRelease()
@@ -1138,7 +1139,7 @@ namespace track.Controllers
 
         public List<featureAttr> queryFeatureAttr()
         {
-      /*     string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
+          string coonString = @"SERVER=TFSOFFICEWH;UID=jipinshi;Trusted_Connection=Yes;APP=Microsoft Office 2013;WSID=Agile-Tracking";
             SqlConnection connection = new SqlConnection(coonString);
             connection.Open();
             // MessageBox.Show("Open database success!");
@@ -1182,8 +1183,8 @@ namespace track.Controllers
                 list.Add(f1);
             }
             return list;
-      */      
-            featureAttr f1 = new featureAttr();
+           
+      /*      featureAttr f1 = new featureAttr();
             f1.featureName = "feature 1";
             f1.pair = new List<personReleasePair>();
             personReleasePair t1 = new personReleasePair();
@@ -1236,7 +1237,7 @@ namespace track.Controllers
             list.Add(f2);
             list.Add(f3);
             return list;
-           
+       */    
         }
     }
 
